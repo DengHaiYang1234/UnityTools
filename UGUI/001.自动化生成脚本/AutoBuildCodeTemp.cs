f61b3ad8ac5bab11b20c6ceccb2777a517133a8f
DengHaiYang1234 <429078038@qq.com>
2018-09-11 22:15:41 +0800

    2018.9.11
    自动化脚本生成

---
+++ UGUI/001.自动化生成脚本/AutoBuildCodeTemp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class AutoBuildCodeTemp
+{
+	public static string UIClass =
+		@"using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+public class #类名#:MonoBehaviour
+{
+	#成员#
+	//auto
+	public void Start()
+	{
+		#查找#
+	}
+
+
+	
+}
+";
+
+
+}
+
+
